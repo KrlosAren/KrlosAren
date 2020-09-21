35e9856d5e5b60b8c83138efe1883d927e98e7d2
Carlos Lopez Z <krlosaren@gmail.com>
2020-09-21 19:33:23 -0300

    add pages make simple, menu and function

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ import './assets/styles/main.scss';
 
 const buttonMenu = document.getElementById('buttonMenu');
 const menuContainer = document.getElementById('menu__hidden');
+const mainContainer = document.getElementById('main__container');
 
 buttonMenu.addEventListener('click', () => {
-  menuContainer.classList.toggle('hidden');
+  menuContainer.classList.toggle('hiddenMenu');
+  mainContainer.classList.toggle('hidden');
 });
 
 console.log(buttonMenu);

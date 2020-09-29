fdfa0651e9cf6893ce1de329382be760b037b0a6
Carlos Lopez Z <krlosaren@gmail.com>
2020-09-28 22:10:11 -0300

    add post and projects

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import menu from './js/components/menu';
 import changeHand from './js/components/indicators';
 import letterAnimation from './js/components/letters';
 
+import openModal from './js/components/openModal';
+
 import './assets/styles/main.scss';
 
 const letterButton = document.getElementById('buttonPunch');
@@ -10,3 +12,11 @@ window.addEventListener('load', menu);
 window.addEventListener('scroll', changeHand);
 
 letterButton.addEventListener('click', letterAnimation);
+
+const loadFunction = () => {
+  if (window.location.pathname === '/Projects.html') {
+    openModal();
+  }
+};
+
+window.addEventListener('load', loadFunction);
---
+++ src/js/components/openModal.jsx
@@ -0,0 +1,9 @@
+
+const openModal = () => {
+  const modalButton = document.getElementById('modal');
+
+  modalButton.addEventListener('click', console.log('hola'));
+
+};
+
+export default openModal;
